02c9ebc4cbae88fb8e7edad2b5f72ac4a0d6fc92
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-08-11 11:50:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static("/"));
 
 app.use(express.json());
 app.engine("html", es6Renderer);
-app.set("views", ".html");
+app.set("views", "/");
 app.set("view engine", "html");
 
 //home page
