ccf5847fdfb6c9d9c3973284687da448a5575094
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-08-11 15:43:34 -0500

    heroku config vars

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
+const clientId = process.env.clientId;
+const clientSecret = process.env.clientSecret;
 const app = express();
 const es6Renderer = require("express-es6-template-engine");
 
