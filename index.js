4db1f1dec22a842348067b1177bf44537373db06
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-08-11 11:29:29 -0500

    node addition

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const es6Renderer = require("express-es6-template-engine");
+const home = require("index");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const store = new SequelizeStore({
+  db: models.sequelize,
+});
+
+app.use(express.static("/"));
+
+app.use(express.json());
+app.engine("html", es6Renderer);
+app.set("views", "./");
+app.set("view engine", "html");
+app.use("/", home);
+
+//home page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//faq page
+app.get("/faq", (req, res) => {
+  res.render("faqs");
+});
+//gallery page
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+//playlist page
+app.get("/playlist", (req, res) => {
+  res.render("playlist");
+});
+//registry page
+app.get("/registry", (req, res) => {
+  res.render("registry");
+});
+//rsvp page
+app.get("/rsvp", (req, res) => {
+  res.render("rsvp");
+});
+//schedule page
+app.get("/schedule", (req, res) => {
+  res.render("schedule");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
