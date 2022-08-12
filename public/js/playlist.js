f5807da89f0406604c8d6cc4f930c7cd51f235fc
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-08-12 09:26:15 -0500

    Update playlist.js

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -10,7 +10,7 @@ let currentPlaylist = [];
 
 const getKey = async () => {
   const result = await fetch(
-    "https://stacy-adair-wedding-website.herokuapp.com/server/config",
+    "https://stacy-adair-wedding-website.herokuapp.com/config",
     {
       method: "GET",
       headers: {
